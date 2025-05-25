3c53c8901e83a990bcf105feeb438f488e3fb6a4
gromif <alexander.kobrys@proton.me>
2025-05-25 22:04:26 +0300

    Integrate `nix-flatpak` flake

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flatpaks.url = "github:gmodena/nix-flatpak/?ref=latest";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = inputs@{ self, nixpkgs, flatpaks, ... }: {
     nixosConfigurations.apollo = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        flatpaks.nixosModules.nix-flatpak
         
         ./configuration.nix
       ];
