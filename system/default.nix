0a175b7dc71c1dd45d0b4fc9b3327e540642979e
gromif <alexander.kobrys@proton.me>
2025-05-25 18:41:35 +0300

    Fix flathub repo systemd service

--- system/default.nix
+++ system/default.nix
@@ -33,6 +33,7 @@
   # Enable Flatpak Support
   services.flatpak.enable = true;
   systemd.services.flathub-repo = { # Set-up Flathub by default
+    after = [ "network.target" ];
     wantedBy = [ "multi-user.target" ];
     path = [ pkgs.flatpak ];
     script = "flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo";
