9c6101b1e86ae90b4f57628593a8e222a7123a1e
gromif <alexander.kobrys@proton.me>
2025-05-31 21:19:42 +0300

    update deprecated options

--- home/gnome/default.nix
+++ home/gnome/default.nix
@@ -9,8 +9,8 @@
   ];
   
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   
   # Install GNOME Essentials
   environment.systemPackages = with pkgs; [
