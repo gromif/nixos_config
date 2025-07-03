ebd3de0f8f119121ac314cd185b36d889b66ed59
gromif <alexander.kobrys@proton.me>
2025-07-03 12:48:10 +0300

    Add custom Random background implementation

--- home/services/random-background.nix
+++ home/services/random-background.nix
@@ -1,11 +1,39 @@
 # Home - Services - Random Background
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
+let
+  wallpapersFolder = "${config.home.homeDirectory}/Pictures/wallpapers";
+  interval = "1h";
+  randomWallpaperPkg = pkgs.writeShellApplication {
+  	  name = "random-wallpaper";
+  	  text = ''
+  	    selectedWallpaper=$(find ${wallpapersFolder} -type f | shuf -n 1)
+  	    
+  	    gsettings set org.gnome.desktop.background picture-uri "$selectedWallpaper"
+  	  '';
+  	};
+in
 {
-  services.random-background = {
-    enable = true;
-    imageDirectory = "~/Pictures/wallpapers";
-    interval = "1h";
+  # Install `randomWallpaperPkg` binary
+  home.packages = [ randomWallpaperPkg ];
+  	
+  	# Set up automatic `randomWallpaperPkg`
+  	systemd.user.services.random-wallpaper = {
+  	  Unit.Description = "Set random wallpaper";
+  	  Service = {
+      ExecStart = "${lib.getExe randomWallpaperPkg}";
+  	  };
+  	};
+  
+  	# Set up automatic `randomWallpaperPkg` timer
+  systemd.user.timers.random-wallpaper = {
+    Unit.Description = "Set random wallpaper [Timer]";
+    Timer = {
+      OnBootSec = "${interval}";
+      OnUnitActiveSec = "${interval}";
+      Unit = "random-wallpaper.service";
+    };
+    Install.WantedBy = ["timers.target"];
   };
 }
