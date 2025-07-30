e61f7f4f4194c47347dc00e924b88d311b44312d
gromif <alexander.kobrys@proton.me>
2025-07-30 22:38:26 +0300

    automatically load an RGB profile at boot

--- user/alex/config/openrgb/default.nix
+++ user/alex/config/openrgb/default.nix
@@ -1,22 +1,41 @@
-# User - Alex - Config - OpenRGB - Profiles
+# User - Alex - Config - OpenRGB
 
 
-{ pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 let
-  profile = "Latte";
-  disabledProfile = "Disabled";
+  pkg-openrgb = pkgs.openrgb-with-all-plugins;
+  profilesDir = "${config.users.users.alex.home}/.config/OpenRGB";
+  
+  rgb-next = pkgs.writeShellApplication {
+    name = "rgb-next";
+    runtimeInputs = with pkgs; [ pkg-openrgb findutils coreutils ];
+    text = ''
+      targetProfile=$(find "${profilesDir}/" -type l -name "*.orp" | shuf -n 1)
+      echo "The target profile is $targetProfile"
+      openrgb -p "$targetProfile"
+    '';
+  };
+  rgb-next-systemd = "rgb-next";
 in
 {
+  environment.systemPackages = [ pkg-openrgb rgb-next ];
   # Set up UDEV rules
-  services.udev.packages = with pkgs; [ openrgb-with-all-plugins ];
+  services.udev.packages = [ pkg-openrgb ];
   boot.kernelModules = [ "i2c-dev" "i2c-piix4" ];
   
-  systemd.user.services = {
-    openrgb-boot = {
-      path = [ pkgs.openrgb-with-all-plugins ];
-      script = "openrgb --profile ${profile}";
-      wantedBy = [ "default.target" "reboot.target" ];
+  # Set a random profile on boot
+  systemd.user = {
+    services."${rgb-next-systemd}" = {
+      path = [ rgb-next ];
+      script = "rgb-next";
+    };
+    timers."${rgb-next-systemd}" = {
+      timerConfig = {
+        OnBootSec = "1s";
+        Unit = "${rgb-next-systemd}.service";
+      };
+      wantedBy = [ "timers.target" ];
     };
   };
   
