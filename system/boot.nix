f903eac219d344949068a7ad7fb2168f55f41f2f
gromif <alexander.kobrys@proton.me>
2025-08-08 12:07:54 +0300

    add v4l2loopback

--- system/boot.nix
+++ system/boot.nix
@@ -5,13 +5,19 @@
 
 {
   # Kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_15;
   boot.kernelParams = [
     # Main display overdrive
     "video=HDMI-A-2:1920x1080@83"
     # AMDGPU Overclock Support
     "amdgpu.ppfeaturemask=0xffffffff"
   ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+
+  boot.kernelModules = [ "v4l2loopback" ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="VirtualCam" exclusive_caps=1
+  '';
   
   # Bootloader
   boot.loader = {
