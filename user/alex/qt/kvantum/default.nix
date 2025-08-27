21d4303d5fcec764d41cc991c697bebceb6488d0
gromif <alexander.kobrys@proton.me>
2025-08-27 21:19:28 +0300

    add Kvantum.config

--- user/alex/qt/kvantum/default.nix
+++ user/alex/qt/kvantum/default.nix
@@ -3,16 +3,20 @@
 
 { config, pkgs, ... }:
 
+let
+  theme = "KvLibadwaitaDark";
+in
 {
   # Set up basic packages
   environment.systemPackages = with pkgs; [
     kdePackages.qtstyleplugin-kvantum
   ];
   
-  # Declare config files
-  environment.etc = {
-    
-  };
+  # Declare the config file
+  environment.etc."xdg/Kvantum/kvantum.kvconfig".text = ''
+    [General]
+    theme=${theme}
+  '';
   
   # Set up QT theming
   qt.style = "kvantum";
