1d847e8a3de029b00f0183a21c5f1cd80d4a93ab
gromif <alexander.kobrys@proton.me>
2025-09-06 19:27:36 +0300

    declare gnome-text-editor config

---
+++ home/gnome/programs/gnome-text-editor.nix
@@ -0,0 +1,16 @@
+# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+{ lib, ... }:
+
+with lib.hm.gvariant;
+
+{
+  dconf.settings."org/gnome/TextEditor" = {
+    custom-font = "FiraCode Nerd Font weight=450 12";
+    indent-style = "space";
+    restore-session = false;
+    show-line-numbers = true;
+    style-scheme = "peninsula";
+    tab-width = mkUint32 2;
+    use-system-font = false;
+  };
+}
