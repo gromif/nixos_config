7d035d1aa3c7fc08d7e66be45fc66c2eab7850a6
gromif <alexander.kobrys@proton.me>
2025-09-17 22:34:51 +0300

    ref(bottles): simplify systemd.tmpfiles setup

--- user/alex/programs/bottles.nix
+++ user/alex/programs/bottles.nix
@@ -6,36 +6,36 @@
 let
   bottlesRoot = ".local/share/bottles/bottles";
   relativePaths = [
-    "ProgramData/Package Cache/*"
+    "ProgramData/Package Cache"
     
-    "Program Files/Internet Explorer/*"
-    "Program Files/Windows NT/*"
-    "Program Files/Windows Media Player/*"
-    "Program Files (x86)/Internet Explorer/*"
-    "Program Files (x86)/Windows NT/*"
-    "Program Files (x86)/Windows Media Player/*"
+    "Program Files/Internet Explorer"
+    "Program Files/Windows NT"
+    "Program Files/Windows Media Player"
+    "Program Files (x86)/Internet Explorer"
+    "Program Files (x86)/Windows NT"
+    "Program Files (x86)/Windows Media Player"
     
-    "windows/Installer/*"
-    "windows/logs/*"
-    "windows/temp/*"
+    "windows/Installer"
+    "windows/logs"
+    "windows/temp"
     
-    "users/*/Temp/*"
-    "users/*/AppData/Local/CEF/*"
-    "users/*/AppData/Local/CrashReportClient/*"
-    "users/*/AppData/Local/Microsoft/*"
-    "users/*/AppData/Local/NVIDIA Corporation/*"
-    "users/*/AppData/Local/Telemetry/*"
-    "users/*/AppData/Local/Temp/*"
+    "users/*/Temp"
+    "users/*/AppData/Local/CEF"
+    "users/*/AppData/Local/CrashReportClient"
+    "users/*/AppData/Local/Microsoft"
+    "users/*/AppData/Local/NVIDIA Corporation"
+    "users/*/AppData/Local/Telemetry"
+    "users/*/AppData/Local/Temp"
     
-    "users/*/AppData/LocalLow/Microsoft/*"
+    "users/*/AppData/LocalLow/Microsoft"
   ];
   
   rules = map (f: 
-    "R \"%h/${bottlesRoot}/*/drive_c/${f}\" - - - - -"
+    "R \"%h/${bottlesRoot}/*/drive_c/${f}/*\" - - - 0 -"
   ) relativePaths;
   
   extraRules = [ 
-    "R \"%h/.local/share/bottles/temp/*\" - - - - -"
+    "R \"%h/.local/share/bottles/temp/*\" - - - 0 -"
   ];
 in
 {
