429908d37f95a2b52c8f71988e81f476a8534062
gromif <alexander.kobrys@proton.me>
2025-09-17 20:25:22 +0300

    extract user DE config

--- user/alex/default.nix
+++ user/alex/default.nix
@@ -11,6 +11,7 @@ let
 in
 {
   imports = [
+    ./desktop_environment.nix
     ./gaming.nix
     ./mimetypes.nix
     ./virtualisation.nix
@@ -29,55 +30,9 @@ in
     shell = pkgs.zsh;
   };
   
-  # ============================================================================================================
-  # Desktop Environment
-  # ============================================================================================================
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
   environment.systemPackages = with pkgs; [
     megasync
-    
-    #Theming
-    kdePackages.ocean-sound-theme
-    adw-gtk3
   ];
-  
-  # ============================================================================================================
-  # Desktop Environment [Exclude]
-  # ============================================================================================================
-  environment.gnome.excludePackages = with pkgs; [
-    orca
-    decibels
-    evince
-    file-roller
-    geary
-    epiphany
-    gnome-contacts
-    gnome-music
-    gnome-software
-    gnome-system-monitor
-    gnome-connections
-    gnome-tour
-    simple-scan
-    yelp
-  ];
-  
-  # ============================================================================================================
-  # Desktop Environment [Fixes]
-  # ============================================================================================================
-  # Fix for Nautilus media details page
-  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
-    gst-plugins-good
-    gst-plugins-bad
-    gst-plugins-ugly
-    gst-libav
-  ]);
-  # Enable numlock in gdm.
-  programs.dconf.profiles.gdm.databases = [{
-    settings."org/gnome/desktop/peripherals/keyboard" = {
-      numlock-state = true;
-    };
-  }];
   
   programs.firefox.enable = true; # Install firefox.
   
---
+++ user/alex/desktop_environment.nix
@@ -0,0 +1,54 @@
+# User - Alex - Desktop Environment
+
+
+{ config, pkgs, lib, ... }:
+
+{
+  # ============================================================================================================
+  # Desktop Environment
+  # ============================================================================================================
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  environment.systemPackages = with pkgs; [
+    # Theming
+    kdePackages.ocean-sound-theme
+    adw-gtk3
+  ];
+  
+  # ============================================================================================================
+  # Desktop Environment [Exclude]
+  # ============================================================================================================
+  environment.gnome.excludePackages = with pkgs; [
+    orca
+    decibels
+    evince
+    file-roller
+    geary
+    epiphany
+    gnome-contacts
+    gnome-music
+    gnome-software
+    gnome-system-monitor
+    gnome-connections
+    gnome-tour
+    simple-scan
+    yelp
+  ];
+  
+  # ============================================================================================================
+  # Desktop Environment [Fixes]
+  # ============================================================================================================
+  # Fix for Nautilus media details page
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+    gst-plugins-good
+    gst-plugins-bad
+    gst-plugins-ugly
+    gst-libav
+  ]);
+  # Enable numlock in gdm.
+  programs.dconf.profiles.gdm.databases = [{
+    settings."org/gnome/desktop/peripherals/keyboard" = {
+      numlock-state = true;
+    };
+  }];
+}
