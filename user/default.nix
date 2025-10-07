d7a1b0f43e2478b5a8ca7a5c49d293d91541e070
gromif <alexander.kobrys@proton.me>
2025-10-07 18:20:32 +0300

    force ssh keys recreation

--- user/default.nix
+++ user/default.nix
@@ -17,8 +17,8 @@
   # Set up ssh keys
   systemd.tmpfiles.rules = [
     "d /root/.ssh 0700 root root - -"
-    "L /root/.ssh/known_hosts - - - - ${config.sops.secrets."ssh/root/known_hosts".path}"
-    "L /root/.ssh/id_ed25519 - - - - ${config.sops.secrets."ssh/root/id_ed25519".path}"
-    "L /root/.ssh/id_ed25519.pub - - - - ${config.sops.secrets."ssh/root/id_ed25519_pub".path}"
+    "L+ /root/.ssh/known_hosts - - - - ${config.sops.secrets."ssh/root/known_hosts".path}"
+    "L+ /root/.ssh/id_ed25519 - - - - ${config.sops.secrets."ssh/root/id_ed25519".path}"
+    "L+ /root/.ssh/id_ed25519.pub - - - - ${config.sops.secrets."ssh/root/id_ed25519_pub".path}"
   ];
 }
