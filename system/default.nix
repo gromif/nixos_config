5a08b3cd639613d0005a1e77507a4589b6bba3c7
gromif <alexander.kobrys@proton.me>
2025-10-16 19:54:11 +0300

    add `sox` system pkg

--- system/default.nix
+++ system/default.nix
@@ -48,6 +48,7 @@
     yt-dlp
     exiftool # ExifTool is a platform-independent Perl library plus a command-line application for reading, writing and editing meta information in a wide variety of files
     flac # Library and tools for encoding and decoding the FLAC lossless audio file format
+    sox # Sample Rate Converter for audio
     imagemagick # Software suite to create, edit, compose, or convert bitmap images
     libjxl
     
