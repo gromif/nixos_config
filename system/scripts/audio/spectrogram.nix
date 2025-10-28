a5b9d5909496e4b19b096829c93d8e6f583fb0c7
gromif <alexander.kobrys@proton.me>
2025-10-28 22:32:52 +0200

    add spectrogram audio script

---
+++ system/scripts/audio/spectrogram.nix
@@ -0,0 +1,37 @@
+# Scripts - Audio - Spectrogram
+#
+
+{ pkgs, lib, ... }:
+
+let
+  runtimes = with pkgs; [ parallel sox ];
+  
+  walkScriptName = "aud-walk-spectre";
+  scriptName = "aud-spectre";
+  
+  mainScriptPackage = pkgs.writeShellApplication {
+    name = walkScriptName;
+    runtimeInputs = with pkgs; [ parallel scriptPackage ];
+    text = ''
+      find "$(pwd)" -type f \
+        -name "*.flac" \
+        -o -name "*.wav" \
+        -o -name "*.alac" \
+      | parallel --will-cite '${scriptName} {}'
+    '';
+  };
+  
+  scriptPackage = pkgs.writeShellApplication {
+  	  name = scriptName;
+  	  runtimeInputs = runtimes;
+  	  text = ''
+      filePath="$1"
+      outputFile="''${filePath}.png"
+
+  	    sox "$filePath" -n spectrogram -x 2048 -Y 2048 -z 140 -o "$outputFile"
+  	  '';
+  	};
+in
+{
+	environment.systemPackages = [ mainScriptPackage scriptPackage ];
+}
