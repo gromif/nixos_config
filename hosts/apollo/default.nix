b0e29a1e887c23ffb38100d6744ea49ff74fbd24
gromif <alexander.kobrys@proton.me>
2025-11-13 22:17:32 +0200

    simplify ssh config for apollo

--- hosts/apollo/default.nix
+++ hosts/apollo/default.nix
@@ -20,17 +20,6 @@
   
   users.users.root.hashedPasswordFile = config.sops.secrets.user_root_passwordHash.path;
 
-  systemd.tmpfiles.rules = [
-    # Adjust the home folder mode
-    # "z /root 0700 root root - -"
-  
-    # Set up ssh keys
-    "d /root/.ssh 0700 root root - -"
-    "L+ /root/.ssh/known_hosts - - - - ${config.sops.secrets."ssh/root/known_hosts".path}"
-    "L+ /root/.ssh/id_ed25519 - - - - ${config.sops.secrets."ssh/root/id_ed25519".path}"
-    "L+ /root/.ssh/id_ed25519.pub - - - - ${config.sops.secrets."ssh/root/id_ed25519_pub".path}"
-  ];
-
   users.users.alex.packages = with pkgs; [
     # Terminal
     fastfetch
--- secrets/primary/default.nix
+++ secrets/primary/default.nix
@@ -5,9 +5,18 @@
 {
   sops.defaultSopsFile = ./secrets.yaml;
   sops.secrets = {
-    "ssh/root/known_hosts".sopsFile = ./ssh.yaml;
-    "ssh/root/id_ed25519".sopsFile = ./ssh.yaml;
-    "ssh/root/id_ed25519_pub".sopsFile = ./ssh.yaml;
+    "ssh/root/known_hosts" = {
+      sopsFile = ./ssh.yaml;
+      path = "/root/.ssh/known_hosts";
+    };
+    "ssh/root/id_ed25519" = {
+      sopsFile = ./ssh.yaml;
+      path = "/root/.ssh/id_ed25519";
+    };
+    "ssh/root/id_ed25519_pub" = {
+      sopsFile = ./ssh.yaml;
+      path = "/root/.ssh/id_ed25519.pub";
+    };
     
     "luks/usb1" = {};
     "luks/sd1" = {};
