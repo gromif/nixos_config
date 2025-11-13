313dac35b7947b3927a63e9cdb69cd4c5f5a9dcb
gromif <alexander.kobrys@proton.me>
2025-11-13 11:17:15 +0200

    write authorisedKeys in plaintext

--- hosts/mercury/default.nix
+++ hosts/mercury/default.nix
@@ -17,19 +17,15 @@
     extraGroups = [ "wheel" ];
     hashedPasswordFile = config.sops.secrets."users/warden/hashedPassword".path;
     # Set up SSH allowed public keys per/user
-    openssh.authorizedKeys.keyFiles = [
-      config.sops.secrets."ssh/apollo/public_key".path
-      config.sops.secrets."ssh/galileo/public_key".path
-      config.sops.secrets."ssh/polaris/public_key".path
-    ];    
+    openssh.authorizedKeys.keys = config.users.users.root.openssh.authorizedKeys.keys;
   };
   users.users.root = {
     hashedPasswordFile = config.sops.secrets."users/root/hashedPassword".path;
     # Set up SSH allowed public keys per/user
-    openssh.authorizedKeys.keyFiles = [
-      config.sops.secrets."ssh/apollo/public_key".path
-      config.sops.secrets."ssh/galileo/public_key".path
-      config.sops.secrets."ssh/polaris/public_key".path
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCHwV0zA22QHV1DLpaoJusNjg7xO+COQ2QOZ7EBkMSI alexander.kobrys@proton.me"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPfs0HEb2cuq00UCYgXGJ3U9jaQ4gmNa7vpTw8DgCXV4 u0_a229@localhost"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIKKLMwjIS7/ZR83zk5q3taxnnGdRbc8dUBIR5N3gWBe u0_a135@localhost"
     ];
     packages = with pkgs; [
       tcpdump
--- secrets/mercury/default.nix
+++ secrets/mercury/default.nix
@@ -5,10 +5,6 @@
 {
   sops.defaultSopsFile = ./secrets.yaml;
   sops.secrets = {
-    "ssh/apollo/public_key".sopsFile = ./ssh.yaml;
-    "ssh/galileo/public_key".sopsFile = ./ssh.yaml;
-    "ssh/polaris/public_key".sopsFile = ./ssh.yaml;
-
     "users/root/hashedPassword" = {
       sopsFile = ./users.yaml;
       neededForUsers = true;
