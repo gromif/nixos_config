3249084648c8d6ace17ba2327a94ef93960c8f62
Alex <47805607+gromif@users.noreply.github.com>
2025-11-23 20:15:17 +0200

    Add default.nix configuration for Polaris host

---
+++ hosts/polaris/default.nix
@@ -0,0 +1,22 @@
+# Hosts - Polaris
+
+
+{ config, lib, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    dwarfs
+    nano
+  ];
+
+  # Backup etc flakes instead of failing to activate generation if a file already exists in /etc
+  environment.etcBackupExtension = ".bak";
+  
+  # Common preferences
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  system.stateVersion = "24.05";
+  # time.timeZone = preferences.time.timeZone;
+  # networking.hostName = preferences.hostName;
+}
