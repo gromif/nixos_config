ae00c2c9c3208328921cca6a15544ccd3b97123d
Alex <47805607+gromif@users.noreply.github.com>
2025-11-23 22:41:25 +0100

    Remove duplicate nixOnDroidConfigurations.default

--- flake.nix
+++ flake.nix
@@ -125,11 +125,11 @@
         ];
       };
     };
-  };
-  nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
-    pkgs = import nixpkgs { system = "aarch64-linux"; };
-    modules = [
-      ./hosts/polaris
-    ];
+    nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
+      pkgs = import nixpkgs { system = "aarch64-linux"; };
+      modules = [
+        ./hosts/polaris
+      ];
+    };
   };
 }
