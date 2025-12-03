982468e8867955e8bd8710a3ee6d999a8960db42
Alex <47805607+gromif@users.noreply.github.com>
2025-12-03 14:15:04 +0100

    Configure ZSH with aliases and OhMyZsh

--- hosts/polaris/default.nix
+++ hosts/polaris/default.nix
@@ -21,6 +21,30 @@
     shell = "${lib.getExe pkgs.zsh}";
   };
 
+  # Setup ZSH
+  programs.zsh = {
+	  enable = true;
+	  enableBashCompletion = true;
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
+
+      shellAliases = { # List common aliases
+        ll = "ls -l";
+        # edit = "sudo -e"; # sudo-rs: not yet implemented
+      };
+    
+      histFile = "$HOME/.config/zsh/history"; # History file path
+	  histSize = 10000;
+	};
+	
+	programs.zsh.ohMyZsh = { # OhMyZsh setup
+		enable = true;
+    plugins = [
+      "git"         # also requires `programs.git.enable = true;`
+    ];
+    theme = "robbyrussell"; # preferred shell theme
+	};
+
   # Backup etc flakes instead of failing to activate generation if a file already exists in /etc
   environment.etcBackupExtension = ".bak";
   
