707e3c7cbba01a25a68492aaf622a30f2dd45678
Alex <47805607+gromif@users.noreply.github.com>
2025-12-07 19:09:59 +0100

    Update nixpkgs-stable version to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
     impermanence.url = "github:nix-community/impermanence";
     
     home-manager-unstable = {
