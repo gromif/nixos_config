5da8aa4aef7a1a69fc27ec519c4323f273ddf28e
gromif <alexander.kobrys@proton.me>
2025-12-22 23:47:47 +0200

    retain ssh keys for apollo

--- secrets/apollo/default.nix
+++ secrets/apollo/default.nix
@@ -5,6 +5,22 @@
 {
   sops.defaultSopsFile = ./secrets.yaml;
   sops.secrets = {
+    "ssh/ed25519" = {
+      sopsFile = ./ssh.yaml;
+      path = "/etc/ssh/ssh_host_ed25519_key";
+    };
+    "ssh/ed25519_pub" = {
+      sopsFile = ./ssh.yaml;
+      path = "/etc/ssh/ssh_host_ed25519_key.pub";
+    };
+    "ssh/rsa" = {
+      sopsFile = ./ssh.yaml;
+      path = "/etc/ssh/ssh_host_rsa_key";
+    };
+    "ssh/rsa_pub" = {
+      sopsFile = ./ssh.yaml;
+      path = "/etc/ssh/ssh_host_rsa_key.pub";
+    };
     "ssh/root/known_hosts" = {
       sopsFile = ./ssh.yaml;
       path = "/root/.ssh/known_hosts";
