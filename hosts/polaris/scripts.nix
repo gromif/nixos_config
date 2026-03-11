6bb0a92d331795924fe3faeb174b7bd685e4d2bd
gromif <alexander.kobrys@proton.me>
2026-03-11 21:27:24 +0000

    update ssh config

--- hosts/polaris/scripts.nix
+++ hosts/polaris/scripts.nix
@@ -5,7 +5,6 @@
 
 let
   endpoint = "celestial-warden.tplinkdns.com";
-  port = "31472";
   ukIdFile = "IdentityFile ~/.ssh/id_ed25519_uk";
   polaris-wake = pkgs.writeShellApplication {
     name = "polaris-wake";
@@ -14,23 +13,25 @@ let
       wol -v -h ${endpoint} -p 9 00:25:B3:0C:A5:27
     '';
   };
-  polaris-ssh-torrent = pkgs.writeShellApplication {
-    name = "polaris-ssh-torrent";
-    runtimeInputs = with pkgs; [ openssh ];
-    text = ''
-      ssh -p ${port} -L 8080:localhost:8080 warden@${endpoint}
-    '';
-  };
-  polaris-ssh-router = pkgs.writeShellApplication {
-    name = "polaris-ssh-router";
-    runtimeInputs = with pkgs; [ openssh ];
-    text = ''
-      ssh -p ${port} -L 7134:192.168.0.1:443 root@${endpoint}
-    '';
-  };
 in
 {
   programs.ssh.extraConfig = ''
+    Host router
+      Hostname ${endpoint}
+      Port 31472
+      User root
+      ${ukIdFile}
+      LocalForward 7134 192.168.0.1:443
+      RequestTTY no
+    
+    Host mercury-torrent
+      Hostname ${endpoint}
+      Port 31472
+      User root
+      ${ukIdFile}
+      LocalForward 8080 localhost:8080
+      RequestTTY no
+
     Host apollo-root
       Hostname ${endpoint}
       Port 24942
@@ -57,7 +58,5 @@ in
   '';
   environment.systemPackages = [
     polaris-wake
-    polaris-ssh-torrent
-    polaris-ssh-router
   ];
 }
