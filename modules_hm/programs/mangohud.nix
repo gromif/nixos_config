8c26867e496004caec3af77dcfd6c4e1a7d2f972
gromif <alexander.kobrys@proton.me>
2026-04-09 09:11:35 +0200

    update mangohud config

--- modules_hm/programs/mangohud.nix
+++ modules_hm/programs/mangohud.nix
@@ -1,12 +1,24 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 
 let
   cfg = config.hmfiles.programs.mangohud;
   load_enabled = true;
-  load_values = [ 50 90];
-  load_colors = [ "FFFFFF" "FFAA7F" "CC0000" ];
+  load_values = [
+    50
+    90
+  ];
+  load_colors = [
+    "FFFFFF"
+    "FFAA7F"
+    "CC0000"
+  ];
 in
 {
   options.hmfiles.programs.mangohud = {
@@ -17,35 +29,45 @@ in
     programs.mangohud = {
       enable = true;
       enableSessionWide = true;
-  
+
       settings = {
         preset = 3;
-    
+
+        # SYSTEM
+        vram = true;
+        swap = true;
+
         # GPU
         gpu_load_change = load_enabled;
         gpu_load_value = load_values;
         gpu_load_color = load_colors;
         throttling_status = true;
-    
+
         # CPU
+        cpu_temp = true;
+        cpu_power = true;
         cpu_load_change = load_enabled;
         cpu_load_value = load_values;
         cpu_load_color = load_colors;
-    
+
         # Limit
         fps_limit = [ 90 ];
         show_fps_limit = true;
-    
+
         # Other
+        position = "top-right";
         no_display = true; # Disable / hide the hud by default
-        #vulkan_driver = true;
-        #wine = true;
-        #frame_timing = true;
-    
+        display_server = true;
+        vulkan_driver = true;
+        wine = true;
+        winesync = true;
+        frame_timing = true;
+        present_mode = true;
+
         # Background
         background_alpha = "0.6";
         round_corners = 5;
       };
-    };    
+    };
   };
 }
