fb5294e38a4cf9b7538c9ff438a2767aa271bd37
gromif <alexander.kobrys@proton.me>
2026-04-19 19:04:51 +0200

    add pk10k config

--- modules/shell/shell_module.nix
+++ modules/shell/shell_module.nix
@@ -16,6 +16,7 @@ let
   persistData = {
     zsh = [ "/root/.config/zsh" ];
   };
+  zsh_p10k = ./zsh/p10k.zsh;
 in
 {
   options.nixfiles.system.shell = {
@@ -81,6 +82,20 @@ in
 
         histFile = "$HOME/.config/zsh/history"; # History file path
         histSize = 10000;
+
+        interactiveShellInit = ''
+          # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.config/zsh/.zshrc.
+          # Initialization code that may require console input (password prompts, [y/n]
+          # confirmations, etc.) must go above this block; everything else may go below.
+          if [[ -r "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+            source "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+          fi
+
+          # Created by newuser for 5.9
+
+          # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
+          source ${zsh_p10k}
+        '';
       };
       programs.zsh.ohMyZsh = {
         # OhMyZsh setup
