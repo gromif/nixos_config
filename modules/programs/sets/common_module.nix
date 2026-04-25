2d2cdc8fee2ac9e0ea00622f85644f1c661fc8df
gromif <alexander.kobrys@proton.me>
2026-04-26 01:01:34 +0200

    update common programs

--- modules/programs/sets/common_module.nix
+++ modules/programs/sets/common_module.nix
@@ -11,32 +11,49 @@ let
   cfg = config.nixfiles.programs.sets.common;
 in
 {
-  options.nixfiles.programs.sets.common = mkOption {
-    type = types.bool;
-    default = true;
-    description = "Whether to include common utils.";
+  options.nixfiles.programs.sets.common = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether to include common utils.";
+    };
+    fs-specific = {
+      btrfs = mkEnableOption "btrfs common utils";
+    };
   };
 
-  config = mkIf cfg {
+  config = mkIf cfg.enable {
+    nixfiles.programs.sets.common.fs-specific.btrfs = mkDefault (
+      config.nixfiles.hardware.rootfs == "btrfs"
+    );
+
     environment.variables = {
       EDITOR = "hx";
     };
 
     systemd.tmpfiles.rules = [ "L+ %h/.config/helix/languages.toml - - - - ${./helix_languages.toml}" ];
 
-    environment.systemPackages = with pkgs; [
-      # base91 # Implementation of the base91 utility, providing efficient binary-to-text encoding with better space utilization than Base64
-      ddcutil
-      helix # Post-modern modal text editor
-      nixd # Nix language server
-      nixfmt # Optional: formatter
-      ncdu
-      usbutils # Tools for working with USB devices, such as lsusb
-      util-linux # Set of system utilities for Linux
-      parallel
-      psmisc # Set of small useful utilities that use the proc filesystem (such as fuser, killall and pstree)
-      pwgen # Password generator which creates passwords which can be easily memorized by a human
-    ];
+    environment.systemPackages =
+      with pkgs;
+      [
+        # base91 # Implementation of the base91 utility, providing efficient binary-to-text encoding with better space utilization than Base64
+        ddcutil
+        e2fsprogs # Tools for creating and checking ext2/ext3/ext4 filesystems
+        helix # Post-modern modal text editor
+        nixd # Nix language server
+        nixfmt # Optional: formatter
+        ncdu
+        usbutils # Tools for working with USB devices, such as lsusb
+        util-linux # Set of system utilities for Linux
+        parallel
+        psmisc # Set of small useful utilities that use the proc filesystem (such as fuser, killall and pstree)
+        pwgen # Password generator which creates passwords which can be easily memorized by a human
+        stress-ng
+      ]
+      ++ optionals (cfg.fs-specific.btrfs) [
+        btrfs-progs
+        compsize
+      ];
 
     programs.htop = {
       enable = true;
