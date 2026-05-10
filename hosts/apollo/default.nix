da0fa17cd2beee6b9a0f35b9981805b071a2b557
gromif <alexander.kobrys@proton.me>
2026-05-10 12:42:41 +0200

    declare LACT settings

--- hosts/apollo/default.nix
+++ hosts/apollo/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware-configuration.nix
     ./crypttab.nix
+    ./vfio.nix
   ];
 
   time.timeZone = "Europe/Berlin";
@@ -181,4 +182,42 @@
   boot.kernelParams = [
     "video=HDMI-A-1:1920x1080@83"
   ];
+
+  services.lact.settings = {
+    version = 5;
+    daemon = {
+      log_level = "info";
+      admin_group = "wheel";
+      disable_clocks_cleanup = false;
+    };
+    apply_settings_timer = 5;
+    gpus = {
+      "1002:7550-1043:0613-0000:03:00.0" = {
+        fan_control_enabled = false;
+        pmfw_options = {
+          zero_rpm = true;
+        };
+        performance_level = "auto";
+        max_memory_clock = 1350;
+        voltage_offset = -100;
+      };
+    };
+    profiles = {
+      MANAGED = {
+        gpus = {
+          "1002:7550-1043:0613-0000:03:00.0" = {
+            fan_control_enabled = false;
+            pmfw_options = {
+              zero_rpm = true;
+            };
+            performance_level = "auto";
+            max_memory_clock = 1350;
+            voltage_offset = -100;
+          };
+        };
+      };
+    };
+    current_profile = "MANAGED";
+    auto_switch_profiles = false;
+  };
 }
