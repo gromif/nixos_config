efa4af681eb6161d5d25738bc0a085df21f6b2f5
gromif <alexander.kobrys@proton.me>
2026-05-28 11:36:41 +0200

    hm: add fuzzy searchers for options

---
+++ modules_hm/programs/scripts/ns.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+
+let
+  name = "ns";
+  cfg = config.hmfiles.programs.scripts."${name}";
+  runtimeInputs = with pkgs; [
+    fzf
+    nix-search-tv
+  ];
+  nsi = pkgs.writeShellApplication {
+    name = "nsi";
+    inherit runtimeInputs;
+    text = ''
+      nix-search-tv print | fzf --preview 'nix-search-tv preview {}' +s -e \                                                                                   ─╯
+        --header="np | hm" --header-border=rounded
+    '';
+  };
+  ns = pkgs.writeShellApplication {
+    inherit name;
+    inherit runtimeInputs;
+    text = ''
+      nix-search-tv print | fzf --preview 'nix-search-tv preview {}' +s \                                                                                   ─╯
+        --header="np | hm" --header-border=rounded
+    '';
+  };
+in
+{
+  options.hmfiles.programs.scripts."${name}" = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether to include a helper-script to enable nixpkgs fuzzy search";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = [
+      ns
+      nsi
+    ];
+  };
+}
