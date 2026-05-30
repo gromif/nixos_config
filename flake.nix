c75bd74f8e54c4ba0480d38c49589668e9a00a2b
gromif <alexander.kobrys@proton.me>
2026-05-30 22:49:46 +0200

    update stable NIXOS

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
     impermanence.url = "github:nix-community/impermanence";
 
     home-manager-unstable = {
