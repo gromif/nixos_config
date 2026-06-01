19f2be52b08935244e626c7724001056f185f83a
gromif <alexander.kobrys@proton.me>
2026-06-01 10:06:49 +0200

    impermanence: remove NetworkManager state

--- modules/impermanence/impermanence_module.nix
+++ modules/impermanence/impermanence_module.nix
@@ -5,11 +5,16 @@ with lib;
 let
   cfg = config.nixfiles.impermanence;
   persistPath = "/nix/state";
-  inAliases = [ "nixfiles" "impermanence" ];
-  toAliases = [ "environment" "persistence" persistPath ];
-  aliases = map (option:
-    mkAliasOptionModule (inAliases ++ [ option ]) (toAliases ++ [ option ])
-  ) [
+  inAliases = [
+    "nixfiles"
+    "impermanence"
+  ];
+  toAliases = [
+    "environment"
+    "persistence"
+    persistPath
+  ];
+  aliases = map (option: mkAliasOptionModule (inAliases ++ [ option ]) (toAliases ++ [ option ])) [
     "enableDebugging"
     "enableWarnings"
     "directories"
@@ -20,38 +25,58 @@ let
   ];
 in
 {
-  imports = aliases ++ [ ./scripts.nix  ];
-  
+  imports = aliases ++ [ ./scripts.nix ];
+
   options.nixfiles.impermanence = {
     enable = mkEnableOption "enable the Impermanence module";
   };
 
   config = {
     nixfiles.impermanence.enable = mkDefault true;
-    
+
     environment.persistence."${persistPath}" = mkIf (cfg.enable) {
       directories = [
         "/home"
         "/etc/NetworkManager/system-connections" # Manual network configs
-        { directory = "/etc/nixos"; mode = "u=rwx,g=,o="; }
+        {
+          directory = "/etc/nixos";
+          mode = "u=rwx,g=,o=";
+        }
         "/root/.cache/nix"
-        { directory = "/root/.config/sops/age"; mode = "u=rwx,g=,o="; } # Sops keys
+        {
+          directory = "/root/.config/sops/age";
+          mode = "u=rwx,g=,o=";
+        } # Sops keys
         "/var/log"
-        { directory = "/var/lib/AccountsService"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; } # User settings
+        {
+          directory = "/var/lib/AccountsService";
+          user = "colord";
+          group = "colord";
+          mode = "u=rwx,g=rx,o=";
+        } # User settings
         "/var/lib/bluetooth"
         "/var/lib/nixos"
         #"/var/lib/systemd/coredump"
         "/var/lib/power-profiles-daemon"
-        { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+        {
+          directory = "/var/lib/colord";
+          user = "colord";
+          group = "colord";
+          mode = "u=rwx,g=rx,o=";
+        }
         "/var/tmp"
       ];
       files = [
         "/etc/machine-id"
-        "/var/lib/NetworkManager/NetworkManager.state"
-        { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+        {
+          file = "/var/keys/secret_file";
+          parentDirectory = {
+            mode = "u=rwx,g=,o=";
+          };
+        }
       ];
     };
-    
+
     # adjust `persist` mode at runtime
     systemd.tmpfiles.rules = [
       "z ${persistPath} 0750 root root - -"
