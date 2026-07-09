127d4b9f8ff24a24e6746d6f308e4d5a18d14b0e
gromif <alexander.kobrys@proton.me>
2026-07-09 08:22:04 +0200

    disko: mercury config

---
+++ disko/mercury-root.nix
@@ -0,0 +1,66 @@
+let
+  device = "/dev/sda";
+  root = "/nix";
+  persist = "/nix/state";
+in
+{
+  filesystems = {
+    "/".neededForBoot = true;
+    "${root}".neededForBoot = true;
+  };
+
+  disko.devices = {
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "defaults"
+        "mode=755"
+        "size=25%"
+      ];
+    };
+    disk = {
+      sda = {
+        type = "disk";
+        inherit device;
+        content = {
+          type = "mbr";
+          partitions = {
+            boot = {
+              size = "1G";
+              type = "83";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "@nix" = {
+                    mountpoint = root;
+                    mountOptions = [
+                      "compress=zstd:1"
+                      "autodefrag"
+                      "noatime"
+                    ];
+                  };
+                  "@persist" = {
+                    mountpoint = persist;
+                    mountOptions = [
+                      "noatime"
+                    ];
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
