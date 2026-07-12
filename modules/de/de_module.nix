dd3437369359a5503adaa06aa64d8dfdcb4beed3
gromif <alexander.kobrys@proton.me>
2026-07-12 08:21:42 +0200

    nixos: remove gst_all_1.gst-vaapi

--- modules/de/de_module.nix
+++ modules/de/de_module.nix
@@ -21,8 +21,6 @@ let
     gst_all_1.gst-plugins-ugly
     # Plugins to reuse ffmpeg to play almost every video format
     gst_all_1.gst-libav
-    # Support the Video Audio (Hardware) Acceleration API
-    gst_all_1.gst-vaapi
   ];
 in
 {
