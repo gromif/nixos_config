d063225660c6596ad3604a9b08b59f795c0e78c3
gromif <alexander.kobrys@proton.me>
2026-07-12 16:59:50 +0200

    disko: add mercury-rescue

---
+++ disko/mercury-rescue.nix
@@ -0,0 +1,81 @@
+let
+  device = "/dev/sdb";
+  root = "/nix";
+in
+{
+  filesystems = {
+    "/".neededForBoot = true;
+    "${root}".neededForBoot = true;
+  };
+
+  disko.devices = {
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "defaults"
+        "mode=755"
+        "size=25%"
+      ];
+    };
+    disk = {
+      sda = {
+        type = "disk";
+        inherit device;
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1;
+            };
+            ESP = {
+              label = "ESP";
+              name = "ESP";
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "nodev"
+                  "nosuid"
+                  "noexec"
+                  "relatime"
+                ];
+              };
+              priority = 2;
+            };
+            root = {
+              size = "10G";
+              priority = 3;
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/nix" = {
+                    mountpoint = root;
+                    mountOptions = [
+                      "compress=zstd"
+                      "autodefrag"
+                      "noatime"
+                    ];
+                  };
+                };
+              };
+            };
+            usb = {
+              size = "100%";
+              priority = 4;
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
