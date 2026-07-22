7f47c71698e147276ee89e973e930f43e3ed62c1
gromif <alexander.kobrys@proton.me>
2026-07-22 22:26:29 +0200

    nixos: update ssh config

--- modules/services/openssh_module.nix
+++ modules/services/openssh_module.nix
@@ -5,14 +5,12 @@ with lib;
 let
   cfg = config.nixfiles.services.openssh;
   aliases = [
-    (mkAliasOptionModule
-      [ "nixfiles" "services" "openssh" "ports" ]
-      [ "services" "openssh" "ports" ])
+    (mkAliasOptionModule [ "nixfiles" "services" "openssh" "ports" ] [ "services" "openssh" "ports" ])
   ];
 in
 {
   imports = aliases;
-  
+
   options.nixfiles.services.openssh = {
     enable = mkEnableOption "Whether to enable preconfigured OpenSSH.";
     enableEssentials = mkOption {
@@ -22,36 +20,38 @@ in
     };
   };
 
-  config = mkIf cfg.enable (
-    mkMerge [
-      {
-        services.openssh = {
+  config = mkIf cfg.enable (mkMerge [
+    {
+      services.openssh = {
+        enable = true;
+        settings = {
+          X11Forwarding = false;
+          AllowAgentForwarding = false;
+          AllowTcpForwarding = true;
+          UseDNS = false;
+          PasswordAuthentication = false;
+          KbdInteractiveAuthentication = false;
+          PermitRootLogin = "prohibit-password"; # prohibit-password
+        };
+      };
+    }
+
+    (mkIf cfg.enableEssentials {
+      services = {
+        fail2ban = {
           enable = true;
-          settings = {
-            PasswordAuthentication = false;
-            KbdInteractiveAuthentication = false;
-            PermitRootLogin = "prohibit-password"; # prohibit-password
-          };
         };
-      }
-
-     (mkIf cfg.enableEssentials {
-        services = {
-          fail2ban = {
-            enable = true;
-          };
-          endlessh = {
-            enable = true;
-            port = 22;
-            openFirewall = true;
-          }; 
+        endlessh = {
+          enable = true;
+          port = 22;
+          openFirewall = true;
         };
+      };
 
-        # Persist data
-        nixfiles.impermanence.directories = mkIf config.services.fail2ban.enable [
-          "/var/lib/fail2ban"
-        ];
-      })
-    ]
-  );
+      # Persist data
+      nixfiles.impermanence.directories = mkIf config.services.fail2ban.enable [
+        "/var/lib/fail2ban"
+      ];
+    })
+  ]);
 }
