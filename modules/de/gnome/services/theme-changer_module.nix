cd7a499e03d58b7cc2cd1f2ea165fb892f0bedff
gromif <alexander.kobrys@proton.me>
2026-07-25 13:26:49 +0200

    nixos: optimise theme-changer

--- modules/de/gnome/services/theme-changer_module.nix
+++ modules/de/gnome/services/theme-changer_module.nix
@@ -1,35 +1,37 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 with lib;
 
 let
   cfg = config.nixfiles.de.gnome.services.theme-changer;
   themeChanger = pkgs.writeShellApplication {
-  	  name = "theme-changer";
-  	  runtimeInputs = with pkgs; [
-  	    glib
-  	    psmisc
-    	  kdePackages.qtstyleplugin-kvantum
-    	  qbittorrent
-    	  util-linux
-  	  ];
-  	  text = ''
-  	    case $1 in
-          light)
-            kvantummanager --set KvLibadwaita
-            #sudo kvantummanager --set KvLibadwaita
-            gsettings set org.gnome.desktop.interface color-scheme 'default'
-            ;;
-          dark)
-            kvantummanager --set KvLibadwaitaDark
-            #sudo kvantummanager --set KvLibadwaitaDark
-            gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-            ;;
-        esac
-
-        killall -qwr qbittorrent && setsid -f qbittorrent
-  	  '';
-  	};
+    name = "theme-changer";
+    runtimeInputs = with pkgs; [
+      glib
+      psmisc
+      kdePackages.qtstyleplugin-kvantum
+      util-linux
+    ];
+    text = ''
+      	    case $1 in
+              light)
+                kvantummanager --set KvLibadwaita
+                #sudo kvantummanager --set KvLibadwaita
+                gsettings set org.gnome.desktop.interface color-scheme 'default'
+                ;;
+              dark)
+                kvantummanager --set KvLibadwaitaDark
+                #sudo kvantummanager --set KvLibadwaitaDark
+                gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+                ;;
+            esac
+      	  '';
+  };
 in
 {
   options.nixfiles.de.gnome.services.theme-changer = {
@@ -39,7 +41,7 @@ in
   config = mkIf cfg.enable {
     # Install `theme-changer` binary
     environment.systemPackages = [ themeChanger ];
-  	
+
     # Set up automatic theme changer
     systemd.user.services."theme-changer@" = {
       description = "Change System Theme";
@@ -54,22 +56,22 @@ in
     systemd.user.timers."theme-changer-dark" = {
       description = "Dark theme change";
       timerConfig = {
-        OnCalendar = "16:00";
+        OnCalendar = "20:00";
         Unit = "theme-changer@dark.service";
         Persistent = true;
       };
-      wantedBy = ["timers.target"];
+      wantedBy = [ "timers.target" ];
     };
 
     # Set up automatic [light] theme changer timer
     systemd.user.timers."theme-changer-light" = {
       description = "Light theme change";
       timerConfig = {
-        OnCalendar = "08:00";
+        OnCalendar = "07:00";
         Unit = "theme-changer@light.service";
         Persistent = true;
       };
-      wantedBy = ["timers.target"];
+      wantedBy = [ "timers.target" ];
     };
   };
 }
