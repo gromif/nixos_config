abc0f19224b405e451e35fa8763efccb5f17124d
gromif <alexander.kobrys@proton.me>
2026-08-09 23:30:15 +0200

    qbittorrent: update config

--- modules/services/qbittorrent.nix
+++ modules/services/qbittorrent.nix
@@ -13,7 +13,11 @@ in
       LegalNotice.Accepted = true;
       Preferences = {
         WebUI = {
+          AuthSubnetWhitelistEnabled = true;
+          AuthSubnetWhitelist = "10.10.0.1, 10.10.0.2";
           LocalHostAuth = false;
+          Username = "root";
+          Password_PBKDF2 = "@ByteArray(TVKWeyBPesw9FDofThW5RQ==:AqT74nk/MfgMrJdfikJqAdIHwlIzW75Th9udmMYsKRMDC9Uw5nRnvMDVZmVE4sV+8IkJFtuWBecV+wO8KBa7yg==)";
         };
         General.Locale = "en";
       };
