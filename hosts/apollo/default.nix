51358113f0df20a1246c0d5a5c4bfcc9d9c484e2
gromif <alexander.kobrys@proton.me>
2026-08-27 20:38:34 +0200

    apollo(snapper): add essentials

--- hosts/apollo/default.nix
+++ hosts/apollo/default.nix
@@ -100,4 +100,30 @@
   boot.kernelParams = [
     "clearcpuid=umip" # Trade-off: Hypervisor via Proton
   ];
+
+  services.snapper = {
+    persistentTimer = true;
+    configs = {
+      steam_skyrim = {
+        SUBVOLUME = "/home/alex/.steam/steam/steamapps/compatdata/3855877462";
+        ALLOW_USERS = [ "alex" ];
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+        TIMELINE_LIMIT_WEEKLY = 1;
+        TIMELINE_LIMIT_MONTHLY = 0;
+        TIMELINE_LIMIT_QUARTERLY = 0;
+        TIMELINE_LIMIT_YEARLY = 0;
+      };
+      drive_a_music = {
+        SUBVOLUME = "/mnt/drive_a/Music";
+        ALLOW_USERS = [ "alex" ];
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+        TIMELINE_LIMIT_WEEKLY = 1;
+        TIMELINE_LIMIT_MONTHLY = 0;
+        TIMELINE_LIMIT_QUARTERLY = 0;
+        TIMELINE_LIMIT_YEARLY = 0;
+      };
+    };
+  };
 }
