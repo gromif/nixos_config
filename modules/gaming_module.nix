be4c7cee792cf60ed8b3ce25cb82f0606623dd02
gromif <alexander.kobrys@proton.me>
2026-08-31 20:10:48 +0200

    gaming: refactor

--- modules/gaming_module.nix
+++ modules/gaming_module.nix
@@ -9,47 +9,56 @@ with lib;
 
 let
   cfg = config.nixfiles.gaming;
+
+  pkgs_lsfg = pkgs.lsfg-vk;
 in
 {
   options.nixfiles.gaming = {
     enable = mkEnableOption "gaming-optimised environment";
     enableLSFG = mkEnableOption "LSFG support";
+    supportHypervisor = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Whether to support Hypervisor bypass";
+    };
   };
 
-  config = mkIf (cfg.enable) {
-    # Automatically load the NTSYNC module
-    boot.kernelModules = [ "ntsync" ];
-    environment.variables = {
-      WINENTSYNC = 1;
-      PROTON_USE_NTSYNC = 1;
-      PROTON_ENABLE_WAYLAND = 1;
-    };
+  config = mkIf (cfg.enable) (mkMerge [
+    {
+      boot.kernelModules = [ "ntsync" ]; # Automatically load the NTSYNC module
 
-    # Set up Steam
-    programs.steam = {
-      enable = true;
-      extraCompatPackages = with pkgs; [ proton-ge-bin ];
-      package = pkgs.steam.override {
-        extraEnv = {
-          ENABLE_LSFG = mkIf (cfg.enableLSFG) true;
-          MANGOHUD = true;
-          PROTON_ENABLE_WAYLAND = 1;
-        };
+      environment.variables = {
+        WINENTSYNC = 1;
+        PROTON_USE_NTSYNC = 1;
+        PROTON_ENABLE_WAYLAND = 1;
       };
-      extraPackages =
-        with pkgs;
-        [
-          adwaita-icon-theme
-          mangohud
-        ]
-        ++ (if cfg.enableLSFG then (with pkgs; [ lsfg-vk ]) else [ ]);
-    };
 
-    environment.systemPackages = mkIf (cfg.enableLSFG) (
-      with pkgs;
-      [
-        lsfg-vk
-      ]
-    );
-  };
+      # Set up Steam
+      programs.steam = {
+        enable = true;
+        extraCompatPackages = with pkgs; [ proton-ge-bin ];
+        package = pkgs.steam.override {
+          extraEnv = {
+            ENABLE_LSFG = cfg.enableLSFG;
+            MANGOHUD = true;
+            PROTON_ENABLE_WAYLAND = 1;
+          };
+        };
+        extraPackages =
+          with pkgs;
+          [
+            adwaita-icon-theme
+            mangohud
+          ]
+          ++ optionals cfg.enableLSFG [ pkgs_lsfg ];
+      };
+
+      environment.systemPackages = mkIf (cfg.enableLSFG) [ pkgs_lsfg ];
+    }
+    (mkIf cfg.supportHypervisor {
+      boot.kernelParams = [
+        "clearcpuid=umip" # Trade-off: Hypervisor via Proton
+      ];
+    })
+  ]);
 }
